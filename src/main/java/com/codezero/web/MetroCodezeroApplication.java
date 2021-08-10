9a4f7ddff1b24b559563f0a86414242f1bb23612
KyuSahm <gusami32@gmail.com>
2021-08-10 23:40:39 +0900

    spring boot starter project is generated

---
+++ src/main/java/com/codezero/web/MetroCodezeroApplication.java
@@ -0,0 +1,13 @@
+package com.codezero.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetroCodezeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetroCodezeroApplication.class, args);
+	}
+
+}
