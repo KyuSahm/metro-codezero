7ecd609fd163a6e6e70b15eaaca13e80420d5419
KyuSahm <gusami32@gmail.com>
2021-08-11 14:29:58 +0900

    Home Controller file is added and jasper library dependency is added to pom.xml

---
+++ src/main/java/com/codezero/web/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.codezero.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@RequestMapping("/index")
+	public String index() {
+		return "Hello Spring Boot";
+	}
+}
